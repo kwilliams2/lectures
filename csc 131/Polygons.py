530011628c1d70a6dbd8127990a4b895d1843e89
kevin williams <lee50@live.missouristate.edu>
2018-02-09 13:05:36 -0600

    practice

--- csc 131/Polygons.py
+++ csc 131/Polygons.py
@@ -1,4 +1,13 @@
+"""
+t.up() makes it so nothing's drawn
+t.down() will put the pen on the paper and draw
+
+
+"""
+
+
 from turtle import Turtle
+from turtle import Screen
 
 #def regular_polygon(t: Turtle, length: int, num_sides: int):
 
@@ -49,12 +58,29 @@ def octogon(t: Turtle, length: int) -> None:
         t.left(45)
 
 
+def radial_pattern(t: Turtle, n: int, length: int, shape) -> None:
+    """
+
+    :param t: turtle name
+    :param n: number of shapes you wanna draw (it'll make a pattern)
+    :param length: length of shape
+    :param shape: what shape it'll call to draw
+    :return: none
+    """
+    for count in range(n):
+        shape(t, length)
+        t.left(360/n)  #  this is where it makes the adjusts the angle so it doesn't draw over itself
+
 def main() -> int:
     yertle = Turtle()
-    square(yertle, 300)
-    hexagon(yertle, 300)
-    triangel(yertle, 300)
-    octogon(yertle, 300)
+    screen = Screen()
+    yertle.speed(100)
+#    square(yertle, 300)
+#    hexagon(yertle, 300)
+#    triangel(yertle, 300)
+#    octogon(yertle, 300)
+    radial_pattern(yertle, 50, 100, square)
+    screen.exitonclick()
 
 
 if __name__ == '__main__':
